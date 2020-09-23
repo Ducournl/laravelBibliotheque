235051898f69b85e252d0618536e2da823498504
Ducournl <louis.ducournau@le-campus-numerique.fr>
2020-09-23 15:08:01 +0200

    1er seed

--- database/factories/BooksFactory.php
+++ database/factories/BooksFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Books::class, function (Faker $faker) {
     return [
-        'titre' => $faker->word,
+        'titre' => $faker->realText(25),
         'auteur' => $faker->lastName,
         'isbn' => $faker->isbn10,
         'image'=> $faker->imageUrl($width = 270, $height = 440, 'cats')
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -20,14 +20,11 @@ use Illuminate\Support\Str;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->lastName,
-        'firstname' => $faker->firstName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        'newsletter' => $faker->boolean(),
-        'last_seen' => $faker->dateTimeBetween('-6 months'),
-        'admin' => false,
         'created_at' => $faker->dateTimeBetween('-4 years', '-6 months'),
+
     ];
 });
 
